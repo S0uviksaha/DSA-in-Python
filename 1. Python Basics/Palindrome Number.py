964cffa47cb1f9d5640200e3e76f11b4b96b472c
SOUVIK SAHA <souvik21102001@gmail.com>
2025-09-09 00:16:50 +0530

    leetcode 9

---
+++ 1. Python Basics/Palindrome Number.py
@@ -0,0 +1,27 @@
+__package__ = "Python Basics"
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        temp1 = x
+        temp2 = 0
+
+        if x < 0:
+            return False
+
+        while temp1 > 0:
+            r = temp1 % 10
+            temp2 = temp2 * 10 + r
+            temp1 //= 10
+
+        return temp2 == x
+
+if __name__ == "__main__":
+    x = int(input("Enter a number: "))
+
+    solution = Solution()
+    result = solution.isPalindrome(x)
+
+    if result:
+        print(f"{x} is a Palindrome ")
+    else:
+        print(f"{x} is NOT a Palindrome ")
