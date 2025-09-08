4cda80a080d02d6880f2307f93442a45b0ab205b
SOUVIK SAHA <souvik21102001@gmail.com>
2025-09-09 00:52:04 +0530

    leetcode 1431

---
+++ 1. Python Basics/Kids With the Greatest Number of Candies.py
@@ -0,0 +1,30 @@
+__package__ = "Python Basics"
+
+from typing import List
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        maxCandies = 0
+        output = []
+
+        for i in candies:
+            if i > maxCandies:
+                maxCandies = i
+
+        for i in candies:
+            if (i + extraCandies) >= maxCandies:
+                output.append(True)
+            else:
+                output.append(False)
+
+        return output
+
+if __name__ == "__main__":
+    candies = list(map(int, input("Enter candies for each kid (space-separated): ").split()))
+    extraCandies = int(input("Enter extra candies: "))
+
+    solution = Solution()
+    result = solution.kidsWithCandies(candies, extraCandies)
+
+    print("\nResult (True = kid can have greatest number of candies):")
+    print(result)
