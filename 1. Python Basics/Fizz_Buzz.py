cb41e391994b70e038cdf4e1f1efb9424fdee152
SOUVIK SAHA <souvik21102001@gmail.com>
2025-09-08 22:40:38 +0530

    leetcode 412

---
+++ 1. Python Basics/Fizz_Buzz.py
@@ -0,0 +1,25 @@
+__package__ = "Python Basics"
+
+from typing import List
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        output = []
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                output.append("FizzBuzz")
+            elif i % 3 == 0:
+                output.append("Fizz")
+            elif i % 5 == 0:
+                output.append("Buzz")
+            else:
+                output.append(str(i))
+        return output
+
+if __name__ == "__main__":
+    n = int(input("Enter a number: "))
+    solution = Solution()
+    result = solution.fizzBuzz(n)
+
+    print("FizzBuzz result:")
+    print(result)
