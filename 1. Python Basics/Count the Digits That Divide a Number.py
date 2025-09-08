d3016865986ffe7924f3bfa5bd3080547dc3b023
SOUVIK SAHA <souvik21102001@gmail.com>
2025-09-08 23:45:30 +0530

    leetcode 2520

---
+++ 1. Python Basics/Count the Digits That Divide a Number.py
@@ -0,0 +1,22 @@
+__package__ = "Python Basics"
+
+class Solution:
+    def countDigits(self, num: int) -> int:
+        number = num
+        count = 0
+
+        while num > 0:
+            x = num % 10
+            if number % x == 0:
+                count += 1
+            num //= 10
+
+        return count
+
+if __name__ == "__main__":
+    num = int(input("Enter a number: "))
+
+    solution = Solution()
+    result = solution.countDigits(num)
+
+    print(f"Count of digits that evenly divide {num}: {result}")
