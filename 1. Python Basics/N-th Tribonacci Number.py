43d0487e67fccb9c67b863b019f3edcd5821ab39
SOUVIK SAHA <souvik21102001@gmail.com>
2025-09-09 08:44:42 +0530

    leetcode 1137

---
+++ 1. Python Basics/N-th Tribonacci Number.py
@@ -0,0 +1,25 @@
+__package__ = "Python Basics"
+
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n <= 1:
+            return n
+        elif n == 2:
+            return 1
+        
+        f = [0] * (n + 1)
+        f[1] = 1
+        f[2] = 1
+
+        for i in range(3, n + 1):
+            f[i] = f[i - 1] + f[i - 2] + f[i - 3]
+
+        return f[n]
+
+if __name__ == "__main__":
+    n = int(input("Enter a number (n): "))
+
+    solution = Solution()
+    result = solution.tribonacci(n)
+
+    print(f"Tribonacci number at position {n} = {result}")
