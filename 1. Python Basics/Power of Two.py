7bd7433f31d262142e2bae9dce73a520d936b8cd
SOUVIK SAHA <souvik21102001@gmail.com>
2025-09-09 09:22:04 +0530

    leetcode 231

---
+++ 1. Python Basics/Power of Two.py
@@ -0,0 +1,22 @@
+__package__ = "Python Basics"
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n == 1:
+            return True
+
+        if n % 2 != 0 or n <= 0:
+            return False
+        
+        return self.isPowerOfTwo(n // 2) 
+
+if __name__ == "__main__":
+    n = int(input("Enter a number: "))
+
+    solution = Solution()
+    result = solution.isPowerOfTwo(n)
+
+    if result:
+        print(f"{n} is a power of 2")
+    else:
+        print(f"{n} is NOT a power of 2")
