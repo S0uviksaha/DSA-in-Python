2e5b49dacf7d4e0646f97783211ed489d0ae8f04
SOUVIK SAHA <souvik21102001@gmail.com>
2025-09-10 11:06:17 +0530

    leetcode 122

---
+++ 2. List/Best Time to Buy and Sell Stock II.py
@@ -0,0 +1,20 @@
+__package__ = "3. Matrix"
+
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        for i in range(0, len(prices) - 1):
+            if prices[i+1] > prices[i]:
+                profit += (prices[i+1] - prices[i])
+        return profit
+
+
+if __name__ == "__main__":
+    prices = list(map(int, input("Enter stock prices separated by spaces: ").split()))
+
+    solution = Solution()
+    result = solution.maxProfit(prices)
+
+    print("\nMaximum Profit:", result)
